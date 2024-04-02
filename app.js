3ace51119ada8aef8c49565579148d72274d2636
Ar4uk69 <126593592+ar4uk69@users.noreply.github.com>
2024-04-02 20:19:11 +0200

    add side menu

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 var navLinks = document.getElementById("navLinks");
+var faBars = document.getElementById("faBars");
+var faTimes = document.getElementById("faTimes");
 function showMenu() {
-    navLinks.style.right = "0";
+  navLinks.style.right = "0";
+  faBars.style.display = "none";
+  faTimes.style.display = "block";
 }
 function hideMenu() {
-    navLinks.style.right = "-200px";
+  navLinks.style.right = "-40vw";
+  faTimes.style.display = "none";
+  faBars.style.display = "block";
 }
